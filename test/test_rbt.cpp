cd8c659fb7f60ee7a01ee1134477faf7e1f928ee
JeaustinSirias <JEAUSTIN.SIRIAS@ucr.ac.cr>
2020-10-03 23:24:23 -0600

    added multiple tests for several cases in the insertion algorithm

--- test/test_rbt.cpp
+++ test/test_rbt.cpp
@@ -160,32 +160,73 @@ TEST(RedBlackTree, Positive_rbt_node_add_empty_tree)
   EXPECT_EQ(status, rbt_error_codes::RBT_SUCCESS);
 }
 
-TEST(RedBlackTree, Positive_rbt_node_add)
+TEST(RedBlackTree, Positive_rbt_node_add_case_1)
 {
-    std::vector<float> vector{6, 5, 4, 3, 2, 1};
+    //vector{'A', 'C', 'D', 'R', 'Y', 'B'};
+    std::vector<float> vector{65, 67, 68, 82, 89, 66};
     std::shared_ptr<rbt> tree(new rbt());
     tree->rbt_create(vector);
-    std::shared_ptr<rbt_node> node(new rbt_node(9, rbt_color_codes::RBT_COLOR_BLACK));
+    std::shared_ptr<rbt_node> node(new rbt_node(88, rbt_color_codes::RBT_COLOR_RED));
     tree->rbt_node_add(node);
-    tree->rbt_print();
 
-    EXPECT_EQ(tree->root->value, 5);
-    EXPECT_EQ(tree->root->color, rbt_color_codes::RBT_COLOR_BLACK);
-    EXPECT_EQ(tree->root->lc_node->value, 3);
-    EXPECT_EQ(tree->root->lc_node->color, rbt_color_codes::RBT_COLOR_RED);
-    EXPECT_EQ(tree->root->rc_node->value, 6);
-    EXPECT_EQ(tree->root->rc_node->color, rbt_color_codes::RBT_COLOR_BLACK);
-
-    EXPECT_EQ(tree->root->rc_node->rc_node->value, 9);
-    EXPECT_EQ(tree->root->rc_node->rc_node->color, rbt_color_codes::RBT_COLOR_RED);
-
-    EXPECT_EQ(tree->root->lc_node->lc_node->value, 2);
-    EXPECT_EQ(tree->root->lc_node->lc_node->color, rbt_color_codes::RBT_COLOR_BLACK);
-    EXPECT_EQ(tree->root->lc_node->rc_node->value, 4);
-    EXPECT_EQ(tree->root->lc_node->rc_node->color, rbt_color_codes::RBT_COLOR_BLACK);
-    EXPECT_EQ(tree->root->lc_node->lc_node->lc_node->value, 1);
-    EXPECT_EQ(tree->root->lc_node->lc_node->lc_node->color, rbt_color_codes::RBT_COLOR_RED);
+    EXPECT_EQ(tree->root->value, 82);
+    EXPECT_EQ(tree->root->color, false);
+    EXPECT_EQ(tree->root->lc_node->value, 67);
+    EXPECT_EQ(tree->root->lc_node->color, true);
+    EXPECT_EQ(tree->root->lc_node->rc_node->value, 68);
+    EXPECT_EQ(tree->root->lc_node->rc_node->color, false);
+    EXPECT_EQ(tree->root->lc_node->lc_node->value, 66);
+    EXPECT_EQ(tree->root->lc_node->rc_node->color, false);
+    EXPECT_EQ(tree->root->lc_node->lc_node->lc_node->value, 65);
+    EXPECT_EQ(tree->root->lc_node->lc_node->lc_node->color, true);
+    EXPECT_EQ(tree->root->rc_node->value, 89);
+    EXPECT_EQ(tree->root->rc_node->color, false);
+    EXPECT_EQ(tree->root->rc_node->lc_node->value, 88);
+    EXPECT_EQ(tree->root->rc_node->lc_node->color, true);
 }
+
+TEST(RedBlackTree, Positive_rbt_node_add_case_2)
+{
+    //vector{'W', 'X', 'G', 'P'};
+    std::vector<float> vector{87, 88, 71, 80};
+    std::shared_ptr<rbt> tree(new rbt());
+    tree->rbt_create(vector);
+    std::shared_ptr<rbt_node> node(new rbt_node(65, rbt_color_codes::RBT_COLOR_RED));
+    tree->rbt_node_add(node);
+
+    EXPECT_EQ(tree->root->value, 87);
+    EXPECT_EQ(tree->root->color, false);
+    EXPECT_EQ(tree->root->lc_node->value, 71);
+    EXPECT_EQ(tree->root->lc_node->color, true);
+    EXPECT_EQ(tree->root->lc_node->rc_node->value, 80);
+    EXPECT_EQ(tree->root->lc_node->rc_node->color, false);
+    EXPECT_EQ(tree->root->lc_node->lc_node->value, 65);
+    EXPECT_EQ(tree->root->lc_node->rc_node->color, false);
+    EXPECT_EQ(tree->root->rc_node->value, 88);
+    EXPECT_EQ(tree->root->rc_node->color, false);
+}
+
+TEST(RedBlackTree, Positive_rbt_node_add_case_3)
+{
+    //vector{'R', 'D', 'J', 'K'};
+    std::vector<float> vector{82, 68, 74, 75};
+    std::shared_ptr<rbt> tree(new rbt());
+    tree->rbt_create(vector);
+    std::shared_ptr<rbt_node> node(new rbt_node(76, rbt_color_codes::RBT_COLOR_RED));
+    tree->rbt_node_add(node);
+
+    EXPECT_EQ(tree->root->value, 76);
+    EXPECT_EQ(tree->root->color, false);
+    EXPECT_EQ(tree->root->lc_node->value, 74);
+    EXPECT_EQ(tree->root->lc_node->color, true);
+    EXPECT_EQ(tree->root->lc_node->rc_node->value, 75);
+    EXPECT_EQ(tree->root->lc_node->rc_node->color, false);
+    EXPECT_EQ(tree->root->lc_node->lc_node->value, 68);
+    EXPECT_EQ(tree->root->lc_node->rc_node->color, false);
+    EXPECT_EQ(tree->root->rc_node->value, 82);
+    EXPECT_EQ(tree->root->rc_node->color, false);
+}
+
 
 TEST(RedBlackTree, Positive_print)
 {
