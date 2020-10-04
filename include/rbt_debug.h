3e597d6558d8335ef2a9725ed4bf8b58dbe6ab2f
Jose Pablo Apú <josepabloapu@gmail.com>
2020-10-04 15:43:31 -0600

    adds function to check consecutive reds

--- include/rbt_debug.h
+++ include/rbt_debug.h
@@ -10,10 +10,13 @@ public:
     std::vector<std::shared_ptr<rbt_node>> leaves;
     rbt_debug();
     ~rbt_debug();
+    bool is_red(std::shared_ptr<rbt_node> node);
     void update_leaves(std::shared_ptr<rbt> tree);
     void update_leaves_recursive(std::shared_ptr<rbt_node> node);
     int count_black_nodes_to_leaf(std::shared_ptr<rbt_node> node, float value);
     bool test_black_nodes_count(std::shared_ptr<rbt> tree);
+    void detect_consecutive_red_nodes(std::shared_ptr<rbt> tree);
+    std::shared_ptr<rbt_node> detect_consecutive_red_nodes_recursive(std::shared_ptr<rbt_node> node);
 };
 
 #endif
--- src/rbt_debug.cpp
+++ src/rbt_debug.cpp
@@ -14,11 +14,18 @@ rbt_debug::~rbt_debug()
 {
 }
 
+bool rbt_debug::is_red(std::shared_ptr<rbt_node> node)
+{
+    if (node == nullptr)
+        return false;
+
+    return node->color == rbt_color_codes::RBT_COLOR_RED;
+}
+
 void rbt_debug::update_leaves(std::shared_ptr<rbt> tree)
 {
     leaves.clear();
     update_leaves_recursive(tree->root);
-    std::cout << "Number of leaves: " << leaves.size() << std::endl;
 }
 
 void rbt_debug::update_leaves_recursive(std::shared_ptr<rbt_node> node)
@@ -67,7 +74,9 @@ bool rbt_debug::test_black_nodes_count(std::shared_ptr<rbt> tree)
         int current_black_nodes = count_black_nodes_to_leaf(tree->root, it->value);
         if (black_nodes != -1 && current_black_nodes != black_nodes)
         {
-            std::cout << "Error: violation occured" << std::endl;
+            std::cout << "Alert at node: " << it->value << std::endl;
+            std::cout << "Black depth: " << current_black_nodes << std::endl;
+            std::cout << "Error. Violation occured" << std::endl;
             return false;
         }
 
@@ -75,8 +84,48 @@ bool rbt_debug::test_black_nodes_count(std::shared_ptr<rbt> tree)
         {
             black_nodes = current_black_nodes;
         }
-        std::cout << "Black depth: " << current_black_nodes << std::endl;
     }
+    std::cout << "Black depth: " << black_nodes << std::endl;
     std::cout << "Leaves have same black node depth" << std::endl;
     return true;
+}
+
+void rbt_debug::detect_consecutive_red_nodes(std::shared_ptr<rbt> tree)
+{
+    std::shared_ptr<rbt_node> bad_node = detect_consecutive_red_nodes_recursive(tree->root);
+    if (bad_node != nullptr)
+        std::cout << "Error. Violation occured." << std::endl;
+    else
+        std::cout << "No consecutive red nodes found " << std::endl;
+}
+
+std::shared_ptr<rbt_node> rbt_debug::detect_consecutive_red_nodes_recursive(std::shared_ptr<rbt_node> node)
+{
+    if (node == nullptr)
+        return nullptr;
+
+    if (is_red(node))
+    {
+        if (is_red(node->lc_node))
+        {
+            std::cout << "Alert at node: " << node->value << std::endl;
+            return node;
+        }
+
+        if (is_red(node->rc_node))
+        {
+            std::cout << "Alert at node: " << node->value << std::endl;
+            return node;
+        }
+    }
+
+    std::shared_ptr<rbt_node> lc_node = detect_consecutive_red_nodes_recursive(node->lc_node);
+    if (lc_node != nullptr)
+        return node;
+
+    std::shared_ptr<rbt_node> rc_node = detect_consecutive_red_nodes_recursive(node->rc_node);
+    if (rc_node != nullptr)
+        return node;
+
+    return nullptr;
 }
